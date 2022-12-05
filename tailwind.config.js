3caf30a8bf742cdb0a0dc71c60db47bd7db68b68
jinn2u <bsybear623@gmail.com>
2022-12-05 19:43:57 +0900

    [Feature/#1] color token 추가  (#4)
    * feat: color token 추가
    
    * feat: color theme 사용하는 곳을 extend안으로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./src/styles/colors.cjs');
 
 module.exports = {
 	content: [
@@ -6,7 +7,8 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors,
+		},
 	},
-	plugins: [],
 };
