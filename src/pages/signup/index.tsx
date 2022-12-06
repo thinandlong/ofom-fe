64b3746562d86b65e98cff67b1bf6be8863c9be3
MyeongHoonNam <codenmh0822@naver.com>
2022-12-06 23:18:18 +0900

    feat #5: 회원가입 페이지 라우팅

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from '@components/SignUpForm';
+
+const SignUpPage = () => {
+	return (
+		<div className="flex justify-center items-center w-screen h-screen bg-[#D2D4D9]">
+			<SignUpForm />
+		</div>
+	);
+};
+
+export default SignUpPage;
