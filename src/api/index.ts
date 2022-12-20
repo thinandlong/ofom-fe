c7953a9b8d3fe703a8171039b3db9c1c68279bce
MyeongHoonNam <codenmh0822@naver.com>
2022-12-20 23:05:12 +0900

    feat #5: interceptor response error에 대한 예외 처리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,36 @@
 import axios from 'axios';
 
+interface ApiError {
+	status: number;
+	errorCode: string;
+	errors: [];
+	reason: string;
+}
+
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const baseInstance = axios.create({
 	baseURL,
 });
 
-baseInstance.interceptors.response.use((res) => {
-	res.data = res.data.data;
+baseInstance.interceptors.response.use(
+	(res) => res.data,
+	async (err) => {
+		const { response } = err;
+		const {
+			status,
+			data: { errorCode, errors, reason },
+		} = response;
 
-	return res;
-});
+		const formatError: ApiError = {
+			status,
+			errorCode,
+			errors,
+			reason,
+		};
+
+		return Promise.reject(formatError);
+	},
+);
 
 export default baseInstance;
