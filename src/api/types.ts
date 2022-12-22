50e94dc4450e88bd0c346c33e5024ce4a5ef166f
jinn2u <bsybear623@gmail.com>
2022-12-22 22:24:04 +0900

    feat #9: axios interceptor 수정

---
+++ src/api/types.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+	AxiosInstance,
+	AxiosInterceptorManager,
+	AxiosRequestConfig,
+	AxiosResponse,
+} from 'axios';
+
+interface ResponseData {
+	data: any;
+}
+export interface ApiError {
+	status: number;
+	errorCode: string;
+	errors: any[];
+	reason: string;
+}
+
+export interface CustomInstance extends AxiosInstance {
+	interceptors: {
+		request: AxiosInterceptorManager<AxiosRequestConfig>;
+		response: AxiosInterceptorManager<AxiosResponse<ResponseData>>;
+	};
+	getUri(config?: AxiosRequestConfig): string;
+	request<T>(config: AxiosRequestConfig): Promise<T>;
+	get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+	delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+	head<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+	options<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+	post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+	put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+	patch<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+}
