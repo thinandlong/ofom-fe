5cc2afd86251c857a021d20491a9c798c94b94e6
jinn2u <bsybear623@gmail.com>
2022-12-30 14:54:17 +0900

    feat #9: prettier설정은 eslint.json이 아닌 prettier.json에서 한다, useTabs 속성을 false로 수정

--- src/components/SignUpApiErrorTxt/index.tsx
+++ src/components/SignUpApiErrorTxt/index.tsx
@@ -3,11 +3,11 @@ import { ApiError } from '@api/types';
 
 interface SignUpApiErrorProps extends Pick<ApiError, 'errorCode'> {}
 const SignUpApiErrorTxt = ({ errorCode }: SignUpApiErrorProps) => {
-	return (
-		<span>
-			{errorCode === 'USERNAME_ALREADY_EXISTS_ERROR' &&
-				'이미 존재하는 사용자 이름입니다'}
-		</span>
-	);
+  return (
+    <span>
+      {errorCode === 'USERNAME_ALREADY_EXISTS_ERROR' &&
+        '이미 존재하는 사용자 이름입니다'}
+    </span>
+  );
 };
 export default SignUpApiErrorTxt;
