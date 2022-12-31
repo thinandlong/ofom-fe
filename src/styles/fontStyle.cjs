55002f07108d1b42f469459cafea76b3fe14f657
jinn2u <bsybear623@gmail.com>
2022-12-31 18:34:16 +0900

    feat #13: lint 적용

--- src/styles/fontStyle.cjs
+++ src/styles/fontStyle.cjs
@@ -1,165 +1,165 @@
 const fontSize = {
-	title_1_BD: [
-		'48px',
-		{
-			fontWeight: '700',
-			lineHeight: '100%',
-		},
-	],
-	title_2_BD: [
-		'36px',
-		{
-			fontWeight: '700',
-			lineHeight: '120%',
-		},
-	],
-	title_3_BD: [
-		'28px',
-		{
-			fontWeight: '700',
-			lineHeight: '124%',
-		},
-	],
-	title_4_BD: [
-		'24px',
-		{
-			fontWeight: '700',
-			lineHeight: '128%',
-		},
-	],
-	title_1_SB: [
-		'40px',
-		{
-			fontWeight: '600',
-			lineHeight: '120%',
-		},
-	],
-	title_2_SB: [
-		'36px',
-		{
-			fontWeight: '600',
-			lineHeight: '150%',
-		},
-	],
-	title_3_SB: [
-		'28px',
-		{
-			fontWeight: '600',
-			lineHeight: '124%',
-		},
-	],
-	title_4_SB: [
-		'24px',
-		{
-			fontWeight: '600',
-			lineHeight: '128%',
-		},
-	],
-	body_1_BD: [
-		'20px',
-		{
-			fontWeight: '700',
-			lineHeight: '150%',
-		},
-	],
-	body_1_SB: [
-		'20px',
-		{
-			fontWeight: '600',
-			lineHeight: '150%',
-		},
-	],
-	body_1_MD: [
-		'20px',
-		{
-			fontWeight: '500',
-			lineHeight: '150%',
-		},
-	],
-	body_1_RG: [
-		'20px',
-		{
-			fontWeight: '400',
-			lineHeight: '150%',
-		},
-	],
-	body_2_BD: [
-		'16px',
-		{
-			fontWeight: '700',
-			lineHeight: '150%',
-		},
-	],
-	body_2_SB: [
-		'16px',
-		{
-			fontWeight: '600',
-			lineHeight: '150%',
-		},
-	],
-	body_2_MD: [
-		'16px',
-		{
-			fontWeight: '500',
-			lineHeight: '150%',
-		},
-	],
-	body_2_RG: [
-		'16px',
-		{
-			fontWeight: '400',
-			lineHeight: '150%',
-		},
-	],
-	body_3_BD: [
-		'14px',
-		{
-			fontWeight: '700',
-			lineHeight: '150%',
-		},
-	],
-	body_3_SB: [
-		'14px',
-		{
-			fontWeight: '600',
-			lineHeight: '150%',
-		},
-	],
-	body_3_MD: [
-		'14px',
-		{
-			fontWeight: '500',
-			lineHeight: '150%',
-		},
-	],
-	caption_1_SB: [
-		'12px',
-		{
-			fontWeight: '600',
-			lineHeight: '140%',
-		},
-	],
-	caption_1_RG: [
-		'12px',
-		{
-			fontWeight: '400',
-			lineHeight: '140%',
-		},
-	],
-	caption_2_SB: [
-		'10px',
-		{
-			fontWeight: '600',
-			lineHeight: '140%',
-		},
-	],
-	caption_2_RG: [
-		'10px',
-		{
-			fontWeight: '400',
-			lineHeight: '140%',
-		},
-	],
+  title_1_BD: [
+    '48px',
+    {
+      fontWeight: '700',
+      lineHeight: '100%',
+    },
+  ],
+  title_2_BD: [
+    '36px',
+    {
+      fontWeight: '700',
+      lineHeight: '120%',
+    },
+  ],
+  title_3_BD: [
+    '28px',
+    {
+      fontWeight: '700',
+      lineHeight: '124%',
+    },
+  ],
+  title_4_BD: [
+    '24px',
+    {
+      fontWeight: '700',
+      lineHeight: '128%',
+    },
+  ],
+  title_1_SB: [
+    '40px',
+    {
+      fontWeight: '600',
+      lineHeight: '120%',
+    },
+  ],
+  title_2_SB: [
+    '36px',
+    {
+      fontWeight: '600',
+      lineHeight: '150%',
+    },
+  ],
+  title_3_SB: [
+    '28px',
+    {
+      fontWeight: '600',
+      lineHeight: '124%',
+    },
+  ],
+  title_4_SB: [
+    '24px',
+    {
+      fontWeight: '600',
+      lineHeight: '128%',
+    },
+  ],
+  body_1_BD: [
+    '20px',
+    {
+      fontWeight: '700',
+      lineHeight: '150%',
+    },
+  ],
+  body_1_SB: [
+    '20px',
+    {
+      fontWeight: '600',
+      lineHeight: '150%',
+    },
+  ],
+  body_1_MD: [
+    '20px',
+    {
+      fontWeight: '500',
+      lineHeight: '150%',
+    },
+  ],
+  body_1_RG: [
+    '20px',
+    {
+      fontWeight: '400',
+      lineHeight: '150%',
+    },
+  ],
+  body_2_BD: [
+    '16px',
+    {
+      fontWeight: '700',
+      lineHeight: '150%',
+    },
+  ],
+  body_2_SB: [
+    '16px',
+    {
+      fontWeight: '600',
+      lineHeight: '150%',
+    },
+  ],
+  body_2_MD: [
+    '16px',
+    {
+      fontWeight: '500',
+      lineHeight: '150%',
+    },
+  ],
+  body_2_RG: [
+    '16px',
+    {
+      fontWeight: '400',
+      lineHeight: '150%',
+    },
+  ],
+  body_3_BD: [
+    '14px',
+    {
+      fontWeight: '700',
+      lineHeight: '150%',
+    },
+  ],
+  body_3_SB: [
+    '14px',
+    {
+      fontWeight: '600',
+      lineHeight: '150%',
+    },
+  ],
+  body_3_MD: [
+    '14px',
+    {
+      fontWeight: '500',
+      lineHeight: '150%',
+    },
+  ],
+  caption_1_SB: [
+    '12px',
+    {
+      fontWeight: '600',
+      lineHeight: '140%',
+    },
+  ],
+  caption_1_RG: [
+    '12px',
+    {
+      fontWeight: '400',
+      lineHeight: '140%',
+    },
+  ],
+  caption_2_SB: [
+    '10px',
+    {
+      fontWeight: '600',
+      lineHeight: '140%',
+    },
+  ],
+  caption_2_RG: [
+    '10px',
+    {
+      fontWeight: '400',
+      lineHeight: '140%',
+    },
+  ],
 };
 
 module.exports = fontSize;
