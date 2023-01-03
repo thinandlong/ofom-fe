0847944a6ded90de387c49e856dd52ce64c9474b
jinn2u <bsybear623@gmail.com>
2023-01-03 20:17:08 +0900

    feat #13: signup페이지에서 Header와 Form 컴포넌트 분리

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,11 +1,15 @@
-import SignUpForm from '@components/SignUpForm';
+import Header from './components/Header';
+import SignUpForm from './components/SignUpForm';
 
 const SignUpPage = () => {
-	return (
-		<div className="flex justify-center items-center w-screen h-screen bg-[#D2D4D9]">
-			<SignUpForm />
-		</div>
-	);
+  return (
+    <div className="flex-center screen-full bg-gray100 min-w-1200">
+      <div className="bg-white flex-col-items-center w-1045 h-600 rounded-8">
+        <Header />
+        <SignUpForm />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
