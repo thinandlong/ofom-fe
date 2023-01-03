e69ecdd5a91e92022282eb3bddea099d922fbde2
jinn2u <bsybear623@gmail.com>
2023-01-03 20:18:37 +0900

    feat #13: signup페이지에서 useSignUpForm으로 비즈니스 로직 분리

---
+++ src/pages/signup/hooks/useSignUpForm.ts
@@ -0,0 +1,57 @@
+import { ApiError } from '@api/types';
+import useForm from '@hooks/useForm';
+import { SignUp, SignUpFormInput } from '@mutations/useSignUpMutaion/types';
+import { UseMutateAsyncFunction } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+interface UseSignUpFormProps {
+  mutateAsync: UseMutateAsyncFunction<
+    SignUp,
+    ApiError,
+    SignUpFormInput,
+    unknown
+  >;
+}
+
+const UseSignUpForm = ({ mutateAsync }: UseSignUpFormProps) => {
+  const initialFormState = useMemo(
+    () => ({
+      username: '',
+      password: '',
+      nickname: '',
+    }),
+    [],
+  );
+
+  const { values, handleChange, handleSubmit } = useForm({
+    initialState: initialFormState,
+    validate: ({ username, password, nickname }) => {
+      let usernameError = null;
+      let passwordError = null;
+      let nicknameError = null;
+      if (!username) {
+        usernameError = '이메일을 입력해주세요.';
+      }
+      if (!password) {
+        passwordError = '비밀번호를 입력해주세요.';
+      }
+      if (!nickname) {
+        nicknameError = '닉네임을 입력해주세요.';
+      }
+      return {
+        username: usernameError,
+        password: passwordError,
+        nickname: nicknameError,
+      };
+    },
+    onSubmit: async () => {
+      const { username, password, nickname } = values;
+      await mutateAsync({ username, password, nickname });
+    },
+  });
+  return {
+    handleChange,
+    handleSubmit,
+  };
+};
+export default UseSignUpForm;
