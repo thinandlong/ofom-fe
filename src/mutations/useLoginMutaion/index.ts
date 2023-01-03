4d8770d79c1afa6314763b7ba2a441bc138430aa
jinn2u <bsybear623@gmail.com>
2023-01-03 21:25:42 +0900

    fix #13: useLoginMutation onError 추가

--- src/mutations/useLoginMutaion/index.ts
+++ src/mutations/useLoginMutaion/index.ts
@@ -1,18 +1,23 @@
+/* eslint-disable no-console */
+import { ApiError } from '@api/types';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { loginApi } from './api';
 
 const useLoginMutaion = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const mutaion = useMutation(loginApi, {
-		onSuccess: ({ token }) => {
-			localStorage.setItem('token', token);
-			router.push('/main');
-		},
-	});
+  const mutaion = useMutation(loginApi, {
+    onSuccess: ({ token }) => {
+      localStorage.setItem('token', token);
+      router.push('/main');
+    },
+    onError: ({ errorCode }: ApiError) => {
+      console.error(errorCode, 'errorCode');
+    },
+  });
 
-	return mutaion;
+  return mutaion;
 };
 
 export default useLoginMutaion;
