c9d795b4f97ef6590b8a682bc252009fbd9deb2e
jinn2u <bsybear623@gmail.com>
2023-01-03 21:29:21 +0900

    feat #13: login페이지 컴포넌트 위치 변경, Header 컴포넌트 추가, 비즈니스 로직 분리

---
+++ src/pages/login/components/LoginForm/index.tsx
@@ -0,0 +1,38 @@
+import useLoginMutaion from '@mutations/useLoginMutaion';
+import useLogInForm from '@pages/login/hooks/useLoginForm';
+
+const LoginForm = () => {
+  const { mutateAsync, isLoading } = useLoginMutaion();
+  const { handleChange, handleSubmit } = useLogInForm({ mutateAsync });
+
+  return (
+    <form
+      className=" w-280 mt-43
+      flex-col-items-center
+      [&>input]:placeholder:text-gray400 [&>input]:border-gray400 [&>input]:border-b [&>input]:w-full [&>input]:h-31 [&>input]:outline-0 [&>input:not(:first-of-type)]:mt-32"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        name="username"
+        onChange={handleChange}
+        placeholder="이름"
+      />
+      <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        placeholder="비밀번호"
+      />
+      <button
+        className="h-56 text-white rounded-8 w-160 mt-75 bg-gray300 text-body_1_BD"
+        type="submit"
+        disabled={isLoading}
+      >
+        로그인
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,15 @@
-import LoginForm from '@components/LoginForm';
+import Header from './components/Header';
+import LoginForm from './components/LoginForm';
 
 const LoginPage = () => {
-	return (
-		<div className="flex justify-center items-center w-screen h-screen bg-[#D2D4D9]">
-			<LoginForm />
-		</div>
-	);
+  return (
+    <div className="flex-center screen-full bg-gray100 min-w-1200">
+      <div className="bg-white flex-col-items-center w-1045 h-600 rounded-8">
+        <Header />
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
