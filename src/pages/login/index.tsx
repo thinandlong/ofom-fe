c48b823bea04b1365a4098eec52b558f96a0d4fe
jinn2u <bsybear623@gmail.com>
2023-01-06 19:11:09 +0900

    fix #13: Wrapper컴포넌트 분리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,13 @@
 import Header from './components/Header';
 import LoginForm from './components/LoginForm';
+import Wrapper from './components/Wrapper';
 
 const LoginPage = () => {
   return (
-    <div className="flex-center screen-full bg-gray100 min-w-1200">
-      <div className="bg-white flex-col-items-center w-1045 h-600 rounded-8">
-        <Header />
-        <LoginForm />
-      </div>
-    </div>
+    <Wrapper>
+      <Header />
+      <LoginForm />
+    </Wrapper>
   );
 };
 
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,14 +1,13 @@
 import Header from './components/Header';
 import SignUpForm from './components/SignUpForm';
+import Wrapper from './components/Wrapper';
 
 const SignUpPage = () => {
   return (
-    <div className="flex-center screen-full bg-gray100 min-w-1200">
-      <div className="bg-white flex-col-items-center w-1045 h-600 rounded-8">
-        <Header />
-        <SignUpForm />
-      </div>
-    </div>
+    <Wrapper>
+      <Header />
+      <SignUpForm />
+    </Wrapper>
   );
 };
 
