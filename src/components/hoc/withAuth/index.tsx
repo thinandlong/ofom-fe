edac09cf8588dba4161c59a863145e48f822c620
MyeongHoonNam <codenmh0822@naver.com>
2023-01-12 20:19:27 +0900

    refactor #17: 인증에 따른 라우팅 처리 로직 위치 수정

--- src/components/hoc/withAuth/index.tsx
+++ src/components/hoc/withAuth/index.tsx
@@ -1,8 +1,17 @@
 import { NextComponentType } from 'next';
+import { useRouter } from 'next/router';
 import { useUser } from '@queries/index';
+import { useEffect } from 'react';
 
 const withAuth = (Component: NextComponentType) => () => {
-  const { data } = useUser();
+  const { data, isLoading } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/login');
+    }
+  }, [data, router, isLoading]);
 
   return data ? <Component /> : null;
 };
--- src/queries/useUserQuery/index.ts
+++ src/queries/useUserQuery/index.ts
@@ -1,16 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
 import { getUserInfoApi } from './api';
 
 const useUser = () => {
   const QUERY_KEY = ['user'];
-  const router = useRouter();
 
   const query = useQuery(QUERY_KEY, getUserInfoApi, {
     retry: false,
-    onError: () => {
-      router.push('/login');
-    },
   });
 
   return query;
