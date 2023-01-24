11a030c0f905c86cc1bb3ea6bb94e814b581c94b
jinn2u <bsybear623@gmail.com>
2023-01-24 15:07:39 +0900

    feat: recoil setting추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Layout } from '@components/index';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Layout>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Layout>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 };
