ed613da8a4c085732caa94d39a203928ed451c35
jinn2u <bsybear623@gmail.com>
2023-01-24 14:57:08 +0900

    feat: icon component style추가

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { lazy, Suspense, useMemo } from 'react';
+import { CSSProperties, lazy, Suspense, useMemo } from 'react';
 import { COLOR_MAPPER, ICON_MAPPER } from './constants';
 
 interface IconProps {
   name: keyof typeof ICON_MAPPER;
   color: keyof typeof COLOR_MAPPER;
   size?: number;
+  style?: CSSProperties;
 }
-const Icon = ({ color, size, name }: IconProps) => {
+const Icon = ({ color, size = 10, name, style = {} }: IconProps) => {
   const IconComponent = useMemo(() => lazy(ICON_MAPPER[name]!), [name]);
   return (
     <Suspense fallback={<div>loading</div>}>
@@ -15,6 +16,7 @@ const Icon = ({ color, size, name }: IconProps) => {
         className={COLOR_MAPPER[color]}
         width={size}
         height={size}
+        style={style}
       />
     </Suspense>
   );
