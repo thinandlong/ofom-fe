2884e54e3eac4d623b96209f365a0a2c18566896
jinn2u <bsybear623@gmail.com>
2023-01-24 15:10:49 +0900

    feat: checkCircle 아이콘 추가

--- src/components/Icon/constants.ts
+++ src/components/Icon/constants.ts
@@ -1,8 +1,10 @@
 export const COLOR_MAPPER = {
   red700: 'fill-red700',
   green600: 'fill-green600',
+  gray900: 'fill-gray900',
 } as const;
 
 export const ICON_MAPPER = {
   check: () => import('@root/public/assets/svgs/check.svg'),
+  checkCircle: () => import('@root/public/assets/svgs/checkCircle.svg'),
 } as const;
