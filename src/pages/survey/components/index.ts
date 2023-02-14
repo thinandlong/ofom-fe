51d110e0d9747c92a31c81deeb4222ce46a0e839
jinn2u <bsybear623@gmail.com>
2023-02-14 21:53:08 +0900

    feat #20: survey/components 하위 컴포넌트 위치 수정

---
+++ src/pages/survey/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AdditionalTextfield } from './AdditionalTextfield';
+export { default as Textfield } from './Textfield';
+export { default as DurationNotice } from './DurationNotice';
+export { default as Notice } from './Notice';
