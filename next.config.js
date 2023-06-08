d2edb1b196ac991c308fbbbe733b7700c1f8fc22
jinn2u <bsybear623@gmail.com>
2023-06-08 20:42:47 +0900

    [Feature/#19] Icon컴포넌트 개발 (#21)
    * feat: lint 추가
    
    * feat: @svgr/webpack 추가
    
    * feat: gitkeep제거
    
    * feat: root 경로 추가
    
    * feat: svgr webpack 설정
    
    * feat: create Icon component
    
    * feat: icon component style추가

--- next.config.js
+++ next.config.js
@@ -12,6 +12,22 @@ const nextConfig = withTwin({
       },
     ];
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            dimensions: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
   pageExtensions: ['page.tsx'],
 });
 
