80370f4384398d35539dfe52db914a40c1975658
MyeonghoonNam <codenmh0822@naver.com>
2023-10-03 17:01:14 +0900

    feat #41: 설문 페이지 초기 레이아웃 구현

--- src/app/survey/SurveyClient.tsx
+++ src/app/survey/SurveyClient.tsx
@@ -1,7 +1,18 @@
 'use client';
 
+import Wrapper from './components/Wrapper';
+import Controller from './components/Controller';
+import TextFiled from './components/TextField';
+import TextFiledAddButton from './components/TextFieldAddButton';
+
 const SurveyClient = () => {
-  return <div>SurveyClient</div>;
+  return (
+    <Wrapper>
+      <Controller />
+      <TextFiled />
+      <TextFiledAddButton />
+    </Wrapper>
+  );
 };
 
 export default SurveyClient;
